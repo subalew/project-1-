4caaffe3b72900bcc06511d5f1b86ca1d12600af
subalew <subalewma@gmail.com>
2025-01-26 00:48:59 +0200

    Add files via upload

---
+++ project#1/src/components/AboutPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div className = 'main'>
+    <section>
+      <h1>אודות המספרה</h1>
+      <p>המספרה שלנו מציעה שירותי עיצוב שיער וגילוח מקצועיים לגברים</p>
+    </section>
+    </div>
+  );
+};
+
+export default AboutPage;
